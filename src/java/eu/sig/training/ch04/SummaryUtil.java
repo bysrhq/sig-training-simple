9fd85fbc5bd1dd6d650a12673e90df66ee649d17
bysrhq <bysrhq@hotmail.com>
2017-10-11 14:14:00 +0700

    update

---
+++ src/java/eu/sig/training/ch04/SummaryUtil.java
@@ -0,0 +1,16 @@
+package eu.sig.training.ch04;
+
+public class SummaryUtil {
+
+    public static int sum(String counterAccount) {
+        int sum = 0;
+        for (int i = 0; i < counterAccount.length(); i++) {
+            char character = counterAccount.charAt(i);
+            int characterValue = Character.getNumericValue(character);
+            sum = sum + (9 - i) * characterValue;
+        }
+
+        return sum;
+    }
+
+}
